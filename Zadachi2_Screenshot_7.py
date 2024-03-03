3ca4b309d7f628bb40b73626b7b57040fb875c99
Alex Harin <154661998+AlexHarin@users.noreply.github.com>
2024-03-03 22:47:27 +0500

    Add files via upload

---
+++ Zadachi2_Screenshot_7.py
@@ -0,0 +1,32 @@
+class BankAccount:
+    def __init__(self, account_number, owner, balance):
+        self.account_number = account_number
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        """Пополнить счет."""
+        self.balance += amount - (amount * 0.01)
+    def withdraw(self, amount):
+        """Снять средства со счета."""
+        if amount <= self.balance:
+            self.balance -= amount + (amount * 0.01)
+        else:
+            print("Недостаточно средств на счете.")
+
+    def get_balance(self):
+        """Получить баланс счета."""
+        return self.balance
+
+
+account1 = BankAccount("1234567890", "Ахмет Венгалби", 1000)
+account2 = BankAccount("0987654321", "Асхаб Тамаев", 500)
+
+account1.deposit(200)
+account2.deposit(300)
+
+account1.withdraw(150)
+account2.withdraw(250)
+
+print("Баланс счета {}: {}".format(account1.account_number, account1.get_balance()))
+print("Баланс счета {}: {}".format(account2.account_number, account2.get_balance()))
